f20ac8924c09af38484ee264317fdfa7bc6e53e1
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-10-10 12:30:02 -0400

    e

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const DOMselectors = {
   button: document.getElementByID("btn"),
   text: document.querySelector("#text"),
+  box: document.getElementByID("big-black-box"),
+  points: document.querySelectorAll(".point"),
 };
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "This is now a big red box";
+}
+
+backgroundAndText();
+
 console.log();
