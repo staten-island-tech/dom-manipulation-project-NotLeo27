4ab991c8056fa9cf6d86f86ccab55886cbba2b0c
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-10-15 06:48:22 -0400

    e

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const button = document.getElementById("btn");
-
-const DOMselectors = {
-  button: document.getElementByID("btn"),
-  text: document.querySelector("#text"),
-  box: document.getElementByID("big-black-box"),
-  points: document.querySelectorAll(".point"),
+const DOMSelectors = {
+  form: document.getElementByID("user-form"),
+  nameInput: document.getElementById("name"),
+  descriptionInput: document.getElementById("description"),
+  imageUrlInput: document.getElementById("image-url"),
+  gameList: document.getElementById("Game-List")
 };
 
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.innerHTML = "This is now a big red box";
-  text.style.fontSize = "40px";
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const Game = {
+    name: DOMSelectors.nameInput.value,
+    description: DOMSelectors.descriptionInput.value,
+    imageUrl: DOMSelectors.imageUrlInput.value
+  }
+
+  insertUserIntoDOM(Game);
+
+  clearInputFields();
+
 }
 
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+function insertUserIntoDOM(Game) {
 
-console.log(button);
+  const gameCard = document.createElement("div");
+  gameCard.classList.add("card");
+
+  userCard.innerHTML = `
+    <img src="${user.imageUrl} alt="Game Image">
+    <div>
+      <p>Name: ${Game.name}</p>
+      <p>Description: ${Game.description}</p>
+    </div>
+    <button class="remove-btn">Remove</button>
+
+  `;
+
+}
+
+function clearInputFields() {
+  DOMSelectors.nameInput.value = '';
+  DOMSelectors.descriptionInput.value = '';
+  DOMSelectors.imageUrlInput.value = '';
+}
+
+function {
+  
+}
+
+DOMSelectors.form.addEventListener("submit", handleFormSubmit);
