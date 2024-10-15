0dd334954001f1f434947b797ef7f3247f82b9d8
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-10-15 11:47:35 -0400

    e

--- index.js
+++ index.js
@@ -5,47 +5,26 @@ const DOMSelectors = {
   imageUrlInput: document.getElementById("image-url"),
   gameList: document.getElementById("Game-List")
 };
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-
+ 
+function addCard(num) {
   const Game = {
     name: DOMSelectors.nameInput.value,
     description: DOMSelectors.descriptionInput.value,
     imageUrl: DOMSelectors.imageUrlInput.value
   }
-
-  insertUserIntoDOM(Game);
-
-  clearInputFields();
-
+  DOMSelectors.gameList.insertAdjacentElement("beforeend", `<div><h1>${Game.name}</h1><p>${Game.description}</p><img src="${Game.imageUrl}"></div>`)
 }
 
-function insertUserIntoDOM(Game) {
+function run() {
+  DOMSelectors.form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  const gameCard = document.createElement("div");
-  gameCard.classList.add("card");
-
-  userCard.innerHTML = `
-    <img src="${user.imageUrl} alt="Game Image">
-    <div>
-      <p>Name: ${Game.name}</p>
-      <p>Description: ${Game.description}</p>
-    </div>
-    <button class="remove-btn">Remove</button>
-
-  `;
-
+    let n = 0;
+    addCard(n);
+    n += 1;
+  });
 }
 
-function clearInputFields() {
-  DOMSelectors.nameInput.value = '';
-  DOMSelectors.descriptionInput.value = '';
-  DOMSelectors.imageUrlInput.value = '';
-}
+console.log("test")
 
-function {
-  
-}
-
-DOMSelectors.form.addEventListener("submit", handleFormSubmit);
+run()
