f6b5078f9c0089b4e5b59535789f4c3471721954
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-10-17 11:46:46 -0400

    d

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const DOMSelectors = {
   form: document.getElementByID("user-form"),
-  nameInput: document.getElementById("name"),
-  descriptionInput: document.getElementById("description"),
-  imageUrlInput: document.getElementById("image-url"),
-  gameList: document.getElementById("Game-List")
+  name: document.getElementById("name"),
+  desc: document.getElementById("description"),
+  image: document.getElementById("image-url"),
+  gameList: document.getElementById("Game-List"),
 };
- 
+
+function clearInput() {
+  DOMSelectors.name.value = "";
+  DOMSelectors.desc = "";
+}
+
 function addCard(num) {
   const Game = {
     name: DOMSelectors.nameInput.value,
     description: DOMSelectors.descriptionInput.value,
-    imageUrl: DOMSelectors.imageUrlInput.value
-  }
-  DOMSelectors.gameList.insertAdjacentElement("beforeend", `<div><h1>${Game.name}</h1><p>${Game.description}</p><img src="${Game.imageUrl}"></div>`)
+    imageUrl: DOMSelectors.imageUrlInput.value,
+  };
+  DOMSelectors.gameList.insertAdjacentElement(
+    "beforeend",
+    `<div><h1>${Game.name}</h1><p>${Game.description}</p><img src="${Game.imageUrl}"></div>`
+  );
+  document
+    .querySelector(`#b${num}`)
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.querySelector(`#p${num}`).remove();
+    });
 }
 
 function run() {
@@ -25,6 +39,6 @@ function run() {
   });
 }
 
-console.log("test")
+console.log("test");
 
-run()
+run();
